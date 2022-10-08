c2c08163b36b0044e74f68137809bc859e71268f
Daniel Mendes <1816260-Daniel-Mendes@users.noreply.gitlab.com>
2022-10-08 18:37:44 +0200

    fix: Fixed error with BASE_URL

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 // "http://localhost:8888/comem-archidep-php-todo-exercise/", then BASE_URL
 // should be "/comem-archidep-php-todo-exercise/". If you are accessing the
 // application at "http://localhost:8888", then BASE_URL should be "/".
-define('BASE_URL', '/');
+define('BASE_URL', '/comem-archidep-php-todo-exercise/');
 
 // Database connection parameters.
 define('DB_USER', 'todolist');
