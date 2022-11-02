b79046567ae81ddb159f0e909a7293206ed8c6d9
Daniel Mendes <contact@daniel-mendes.ch>
2022-11-02 16:34:39 +0100

    Remove default env value for DB_PASS

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ define('BASE_URL', getenv('TODOLIST_BASE_URL') ?: '/');
 
 // Database connection parameters.
 define('DB_USER', getenv('TODOLIST_DB_USER') ?: 'todolist');
-define('DB_PASS', getenv('TODOLIST_DB_PASS') ?: 'chAngeMeN0w!');
+define('DB_PASS', getenv('TODOLIST_DB_PASS'));
 define('DB_NAME', getenv('TODOLIST_DB_NAME') ?: 'todolist');
 define('DB_HOST', getenv('TODOLIST_DB_HOST') ?: '127.0.0.1');
 define('DB_PORT', getenv('TODOLIST_DB_PORT') ?: '3306');
